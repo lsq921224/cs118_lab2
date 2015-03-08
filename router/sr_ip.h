b6a9334dad19972b50e2b82a77cee8e54afa7fdd
Shengqian Liu <lius@ucla.edu>
2015-03-07 19:28:20 -0800

    added sr_ip.h

---
+++ router/sr_ip.h
@@ -0,0 +1,17 @@
+#ifndef SR_IP_H
+#define SR_IP_H
+
+#include <sys/types.h>
+#include "sr_router.h"
+
+void handle_ip_packet(struct sr_instance* sr,
+		      uint8_t* packet, 
+		      unsigned len);
+
+int send_ip_packet(struct sr_instance* sr, 
+		   uint32_t dest_ip, 
+		   unsigned char protocol, 
+		   uint8_t* packet, 
+		   unsigned len);
+
+#endif
