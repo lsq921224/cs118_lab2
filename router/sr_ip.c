26bf69e5ec08959053d776b4ac87076ab1c82fed
Shengqian Liu <lius@ucla.edu>
2015-03-07 20:09:46 -0800

    sr_ip.c

---
+++ router/sr_ip.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "sr_utils.h"
+#include "sr_if.h"
+#include "sr_ip.h"
+#include "sr_protocol.h"
+#include "sr_icmp.h"
+
+
+void handle_ip_packet(struct sr_instance* sr, uint8_t* packet, unsigned len)
+{
+  struct sr_ip_hdr* ip_header = (struct sr_ip_hdr*)packet;
+  uint32_t src_ip = ip_header->ip_src;
+  uint32_t des_ip = ip_header->ip_dst;
+  
+
+
+
+
+
+
+
+
+}
