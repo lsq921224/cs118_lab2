599ac5351f1f737d5f381fc96bbf654c9912b4bf
Ryan <lsq921224@gmail.com>
2015-03-13 18:59:32 -0700

    Update sr_icmp.h

--- router/sr_icmp.h
+++ router/sr_icmp.h
@@ -40,7 +40,7 @@ void icmp_net_unreachable (sr_instance_t* sr,
 				unsigned len);
 
 void icmp_host_unreachable( sr_instance_t* sr,
-                            uint32_t dest_ip,
+                            uint32_t des_ip,
                             uint32_t src_ip,
                             uint8_t* packet,
                             unsigned len );
